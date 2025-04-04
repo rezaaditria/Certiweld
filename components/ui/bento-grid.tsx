11831dafa096a8f713465caea587048b39c25015
SCr_ <rezaaditria@student.telkomuniversity.ac.id>
2025-04-04 14:58:39 +0700

    minor 2

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -11,7 +11,7 @@ export const BentoGrid = ({
     <div
       className={cn(
         "mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-3",
-        className,
+        className
       )}
     >
       {children}
@@ -35,19 +35,15 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border border-neutral-200 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
-        className,
+        "group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border   p-4 transition duration-200 hover:shadow-xl ",
+        className
       )}
     >
       {header}
       <div className="transition duration-200 group-hover/bento:translate-x-2">
         {icon}
-        <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
-          {title}
-        </div>
-        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
-          {description}
-        </div>
+        <div className="mt-2 mb-2 font-sans font-bold ">{title}</div>
+        <div className="font-sans text-xs font-normal ">{description}</div>
       </div>
     </div>
   );
